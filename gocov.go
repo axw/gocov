348da7b1b5e76a5dc3b3b66d8405ea0d31f59997
Andrew Wilkins <axwalk@gmail.com>
2012-07-03 20:18:33 +0800

    Add short package description for godoc

--- gocov.go
+++ gocov.go
@@ -18,6 +18,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+// Package gocov is a code coverage analysis tool for Go.
 package gocov
 
 import (
