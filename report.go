084f4ada042b45beeeda26decb9f0b4e92435ba5
Andrew Wilkins <axwalk@gmail.com>
2012-07-04 22:17:19 +0800

    Sort functions by coverage percent

--- report.go
+++ report.go
@@ -23,12 +23,51 @@ package gocov
 import (
 	"fmt"
 	"io"
+	"path/filepath"
 	"sort"
 	"text/tabwriter"
 )
 
 type Report struct {
 	packages []*Package
+}
+
+type reportFunction struct {
+	*Function
+	statementsReached int
+}
+
+type reportFunctionList []reportFunction
+
+func (l reportFunctionList) Len() int {
+	return len(l)
+}
+
+// TODO make sort method configurable?
+func (l reportFunctionList) Less(i, j int) bool {
+	var left, right float64
+	if len(l[i].Statements) > 0 {
+		left = float64(l[i].statementsReached) / float64(len(l[i].Statements))
+	}
+	if len(l[j].Statements) > 0 {
+		right = float64(l[j].statementsReached) / float64(len(l[j].Statements))
+	}
+	if left < right {
+		return true
+	}
+	return left == right && len(l[i].Statements) < len(l[j].Statements)
+}
+
+func (l reportFunctionList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+type reverse struct {
+	sort.Interface
+}
+
+func (r reverse) Less(i, j int) bool {
+	return r.Interface.Less(j, i)
 }
 
 // NewReport creates a new report.
@@ -68,20 +107,27 @@ func PrintReport(w io.Writer, r *Report) {
 }
 
 func printPackage(w io.Writer, pkg *Package) {
-	// TODO make sorting configurable.
-	for _, fn := range pkg.Functions {
+	functions := make(reportFunctionList, len(pkg.Functions))
+	for i, fn := range pkg.Functions {
 		reached := 0
 		for _, stmt := range fn.Statements {
 			if stmt.Reached > 0 {
 				reached++
 			}
 		}
+		functions[i] = reportFunction{fn, reached}
+	}
+	sort.Sort(reverse{functions})
+
+	for _, fn := range functions {
+		reached := fn.statementsReached
 		var stmtPercent float64 = 0
 		if len(fn.Statements) > 0 {
 			stmtPercent = float64(reached) / float64(len(fn.Statements)) * 100
 		}
-		fmt.Fprintf(w, "%s\t%s\t%.2f%% (%d/%d)\n",
-			pkg.Name, fn.Name, stmtPercent, reached, len(fn.Statements))
+		fmt.Fprintf(w, "%s/%s:%d\t %s\t %.2f%% (%d/%d)\n",
+			pkg.Name, filepath.Base(fn.File), fn.Line, fn.Name, stmtPercent,
+			reached, len(fn.Statements))
 	}
 }
 
