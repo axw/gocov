axw/gocov

commit 83cda1567fe3be4ec91bd71577ef1f85c5ed6070

Add -timeout flag to "gocov test"
Also, pass remaining args through to "go test".
This may need revisiting if we ever want to
support testing multiple packages at once
(without using "...", std, etc.)

Fixes #34

gocov/main.go
@@ -76,6 +76,8 @@ var (
 		"run", "",
 		"Run only those tests and examples matching the regular "+
 			"expression.")
+	testTimeoutFlag = testFlags.String(
+		"timeout", "", "If a test runs longer than t, panic.")
 	verbose bool
 )
 
@@ -481,8 +483,12 @@ func instrumentAndTest() (rc int) {
 	if *testRunFlag != "" {
 		args = append(args, "-run", *testRunFlag)
 	}
+	if *testTimeoutFlag != "" {
+		args = append(args, "-timeout", *testTimeoutFlag)
+	}
 	instrumentedPackageName := instrumentedPackagePath(packageName)
 	args = append(args, instrumentedPackageName)
+	args = append(args, testFlags.Args()[1:]...)
 	cmd := exec.Command("go", args...)
 	cmd.Env = env
 	cmd.Stdout = os.Stderr

commit 1d769e0858af37201b6ec7858656ad61f96569eb

Fix breakage introduced in 83cda1567fe3be4ec91bd71577ef1f85c5ed6070
(Check args are specified before attempting to slice.)

gocov/main.go
@@ -488,7 +488,9 @@ func instrumentAndTest() (rc int) {
 	}
 	instrumentedPackageName := instrumentedPackagePath(packageName)
 	args = append(args, instrumentedPackageName)
-	args = append(args, testFlags.Args()[1:]...)
+	if testFlags.NArg() > 1 {
+		args = append(args, testFlags.Args()[1:]...)
+	}
 	cmd := exec.Command("go", args...)
 	cmd.Env = env
 	cmd.Stdout = os.Stderr