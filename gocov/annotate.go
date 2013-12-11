e795ceb9f704e98806707e280b6ce70a5bf1f4b8
cwandrews <charlieandrews.cwa@gmail.com>
2013-12-11 09:10:40 -0500

    ran gofmt

--- gocov/annotate.go
+++ gocov/annotate.go
@@ -37,9 +37,9 @@ import (
 const (
 	hitPrefix  = "    "
 	missPrefix = "MISS"
-	RED = "\x1b[31;1m"
-	GREEN = "\x1b[32;1m"
-	NONE = "\x1b[0m"
+	RED        = "\x1b[31;1m"
+	GREEN      = "\x1b[32;1m"
+	NONE       = "\x1b[0m"
 )
 
 var (
@@ -47,188 +47,188 @@ var (
 	annotateCeilingFlag = annotateFlags.Float64(
 		"ceiling", 101,
 		"Annotate only functions whose coverage is less than the specified percentage")
-	annotateColorFlag	= annotateFlags.Bool(
+	annotateColorFlag = annotateFlags.Bool(
 		"color", false,
 		"Differenciate Coverage with color")
-	)
+)
 
-	type packageList []*gocov.Package
-	type functionList []*gocov.Function
+type packageList []*gocov.Package
+type functionList []*gocov.Function
 
-	func (l packageList) Len() int {
-		return len(l)
+func (l packageList) Len() int {
+	return len(l)
+}
+
+func (l packageList) Less(i, j int) bool {
+	return l[i].Name < l[j].Name
+}
+
+func (l packageList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l functionList) Len() int {
+	return len(l)
+}
+
+func (l functionList) Less(i, j int) bool {
+	return l[i].Name < l[j].Name
+}
+
+func (l functionList) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+type annotator struct {
+	fset  *token.FileSet
+	files map[string]*token.File
+}
+
+func percentReached(fn *gocov.Function) float64 {
+	if len(fn.Statements) == 0 {
+		return 0
 	}
-
-	func (l packageList) Less(i, j int) bool {
-		return l[i].Name < l[j].Name
-	}
-
-	func (l packageList) Swap(i, j int) {
-		l[i], l[j] = l[j], l[i]
-	}
-
-	func (l functionList) Len() int {
-		return len(l)
-	}
-
-	func (l functionList) Less(i, j int) bool {
-		return l[i].Name < l[j].Name
-	}
-
-	func (l functionList) Swap(i, j int) {
-		l[i], l[j] = l[j], l[i]
-	}
-
-	type annotator struct {
-		fset  *token.FileSet
-		files map[string]*token.File
-	}
-
-	func percentReached(fn *gocov.Function) float64 {
-		if len(fn.Statements) == 0 {
-			return 0
+	var reached int
+	for _, stmt := range fn.Statements {
+		if stmt.Reached > 0 {
+			reached++
 		}
-		var reached int
-		for _, stmt := range fn.Statements {
-			if stmt.Reached > 0 {
-				reached++
-			}
-		}
-		return float64(reached) / float64(len(fn.Statements)) * 100
+	}
+	return float64(reached) / float64(len(fn.Statements)) * 100
+}
+
+func annotateSource() (rc int) {
+	annotateFlags.Parse(os.Args[2:])
+	if annotateFlags.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "missing coverage file\n")
+		return 1
 	}
 
-	func annotateSource() (rc int) {
-		annotateFlags.Parse(os.Args[2:])
-		if annotateFlags.NArg() == 0 {
-			fmt.Fprintf(os.Stderr, "missing coverage file\n")
-			return 1
-		}
+	var data []byte
+	var err error
+	if filename := annotateFlags.Arg(0); filename == "-" {
+		data, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		data, err = ioutil.ReadFile(filename)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read coverage file: %s\n", err)
+		return 1
+	}
 
-		var data []byte
-		var err error
-		if filename := annotateFlags.Arg(0); filename == "-" {
-			data, err = ioutil.ReadAll(os.Stdin)
+	packages, err := unmarshalJson(data)
+	if err != nil {
+		fmt.Fprintf(
+			os.Stderr, "failed to unmarshal coverage data: %s\n", err)
+		return 1
+	}
+
+	// Sort packages, functions by name.
+	sort.Sort(packageList(packages))
+	for _, pkg := range packages {
+		sort.Sort(functionList(pkg.Functions))
+	}
+
+	a := &annotator{}
+	a.fset = token.NewFileSet()
+	a.files = make(map[string]*token.File)
+
+	var regexps []*regexp.Regexp
+	for _, arg := range annotateFlags.Args()[1:] {
+		re, err := regexp.Compile(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "warning: failed to compile %q as a regular expression, ignoring\n", arg)
 		} else {
-			data, err = ioutil.ReadFile(filename)
+			regexps = append(regexps, re)
 		}
+	}
+	if len(regexps) == 0 {
+		regexps = append(regexps, regexp.MustCompile("."))
+	}
+	for _, pkg := range packages {
+		for _, fn := range pkg.Functions {
+			if percentReached(fn) >= *annotateCeilingFlag {
+				continue
+			}
+			name := pkg.Name + "/" + fn.Name
+			for _, regexp := range regexps {
+				if regexp.FindStringIndex(name) != nil {
+					err := a.printFunctionSource(fn)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "warning: failed to annotate function %q\n", name)
+					}
+					break
+				}
+			}
+		}
+	}
+	return
+}
+
+func (a *annotator) printFunctionSource(fn *gocov.Function) error {
+	// Load the file for line information. Probably overkill, maybe
+	// just compute the lines from offsets in here.
+	setContent := false
+	file := a.files[fn.File]
+	if file == nil {
+		info, err := os.Stat(fn.File)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to read coverage file: %s\n", err)
-			return 1
+			return err
 		}
+		file = a.fset.AddFile(fn.File, a.fset.Base(), int(info.Size()))
+		setContent = true
+	}
 
-		packages, err := unmarshalJson(data)
-		if err != nil {
-			fmt.Fprintf(
-				os.Stderr, "failed to unmarshal coverage data: %s\n", err)
-				return 1
-			}
+	data, err := ioutil.ReadFile(fn.File)
+	if err != nil {
+		return err
+	}
+	if setContent {
+		// This processes the content and records line number info.
+		file.SetLinesForContent(data)
+	}
 
-			// Sort packages, functions by name.
-			sort.Sort(packageList(packages))
-			for _, pkg := range packages {
-				sort.Sort(functionList(pkg.Functions))
-			}
-
-			a := &annotator{}
-			a.fset = token.NewFileSet()
-			a.files = make(map[string]*token.File)
-
-			var regexps []*regexp.Regexp
-			for _, arg := range annotateFlags.Args()[1:] {
-				re, err := regexp.Compile(arg)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "warning: failed to compile %q as a regular expression, ignoring\n", arg)
-				} else {
-					regexps = append(regexps, re)
+	statements := fn.Statements[:]
+	lineno := file.Line(file.Pos(fn.Start))
+	lines := strings.Split(string(data)[fn.Start:fn.End], "\n")
+	linenoWidth := int(math.Log10(float64(lineno+len(lines)))) + 1
+	fmt.Println()
+	for i, line := range lines {
+		// Go through statements one at a time, seeing if we've hit
+		// them or not.
+		//
+		// The prefix approach isn't perfect, as it doesn't
+		// distinguish multiple statements per line. It'll have to
+		// do for now. We could do fancy ANSI colouring later.
+		lineno := lineno + i
+		statementFound := false
+		hit := false
+		for j := 0; j < len(statements); j++ {
+			start := file.Line(file.Pos(statements[j].Start))
+			// FIXME instrumentation no longer records statements
+			// in line order, as function literals are processed
+			// after the body of a function. If/when that's changed,
+			// we can go back to checking just the first statement
+			// in each loop.
+			if start == lineno {
+				statementFound = true
+				if !hit && statements[j].Reached > 0 {
+					hit = true
 				}
+				statements = append(statements[:j], statements[j+1:]...)
 			}
-			if len(regexps) == 0 {
-				regexps = append(regexps, regexp.MustCompile("."))
-			}
-			for _, pkg := range packages {
-				for _, fn := range pkg.Functions {
-					if percentReached(fn) >= *annotateCeilingFlag {
-						continue
-					}
-					name := pkg.Name + "/" + fn.Name
-					for _, regexp := range regexps {
-						if regexp.FindStringIndex(name) != nil {
-							err := a.printFunctionSource(fn)
-							if err != nil {
-								fmt.Fprintf(os.Stderr, "warning: failed to annotate function %q\n", name)
-							}
-							break
-						}
-					}
-				}
-			}
-			return
 		}
-
-		func (a *annotator) printFunctionSource(fn *gocov.Function) error {
-			// Load the file for line information. Probably overkill, maybe
-			// just compute the lines from offsets in here.
-			setContent := false
-			file := a.files[fn.File]
-			if file == nil {
-				info, err := os.Stat(fn.File)
-				if err != nil {
-					return err
-				}
-				file = a.fset.AddFile(fn.File, a.fset.Base(), int(info.Size()))
-				setContent = true
+		color := NONE
+		hitmiss := hitPrefix
+		if statementFound && !hit {
+			hitmiss = missPrefix
+			if *annotateColorFlag {
+				color = RED
 			}
-
-			data, err := ioutil.ReadFile(fn.File)
-			if err != nil {
-				return err
-			}
-			if setContent {
-				// This processes the content and records line number info.
-				file.SetLinesForContent(data)
-			}
-
-			statements := fn.Statements[:]
-			lineno := file.Line(file.Pos(fn.Start))
-			lines := strings.Split(string(data)[fn.Start:fn.End], "\n")
-			linenoWidth := int(math.Log10(float64(lineno+len(lines)))) + 1
-			fmt.Println()
-			for i, line := range lines {
-				// Go through statements one at a time, seeing if we've hit
-				// them or not.
-				//
-				// The prefix approach isn't perfect, as it doesn't
-				// distinguish multiple statements per line. It'll have to
-				// do for now. We could do fancy ANSI colouring later.
-				lineno := lineno + i
-				statementFound := false
-				hit := false
-				for j := 0; j < len(statements); j++ {
-					start := file.Line(file.Pos(statements[j].Start))
-					// FIXME instrumentation no longer records statements
-					// in line order, as function literals are processed
-					// after the body of a function. If/when that's changed,
-					// we can go back to checking just the first statement
-					// in each loop.
-					if start == lineno {
-						statementFound = true
-						if !hit && statements[j].Reached > 0 {
-							hit = true
-						}
-						statements = append(statements[:j], statements[j+1:]...)
-					}
-				}
-				color := NONE
-				hitmiss := hitPrefix
-				if statementFound && !hit {
-					hitmiss = missPrefix
-					if *annotateColorFlag {
-						color = RED
-					}
-				}
-				fmt.Printf("%s%*d %s\t%s%s\n", color, linenoWidth, lineno, hitmiss, line, NONE)
-			}
-			fmt.Println()
-
-			return nil
 		}
+		fmt.Printf("%s%*d %s\t%s%s\n", color, linenoWidth, lineno, hitmiss, line, NONE)
+	}
+	fmt.Println()
+
+	return nil
+}
